83a788c6a61ce195a440c2b3f4ef1bc900d90b19
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-18 21:12:53 +0900

    feat: 로그인 기능 추가

--- backend/src/main/java/com/board/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/board/domain/member/repository/MemberRepository.java
@@ -4,9 +4,12 @@ import com.board.domain.member.entity.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsMemberByNickname(String nickname);
     boolean existsMemberByUsername(String username);
+    Optional<Member> findMemberByUsername(String username);
 
 }
---
+++ backend/src/main/java/com/board/global/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.board.global.security.service;
+
+import com.board.domain.member.entity.Member;
+import com.board.domain.member.repository.MemberRepository;
+import com.board.global.security.dto.AuthPrincipal;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findMemberByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("회원이 존재하지 않습니다."));
+        return new AuthPrincipal(member);
+    }
+
+}
