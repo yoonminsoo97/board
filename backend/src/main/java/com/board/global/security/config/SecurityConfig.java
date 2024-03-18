1ec7cb7bd528f49b32584deda7a28cffd3c22eb3
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-18 21:12:33 +0900

    feat: 로그인 성공, 실패 핸들러 추가

--- backend/src/main/java/com/board/global/security/config/SecurityConfig.java
+++ backend/src/main/java/com/board/global/security/config/SecurityConfig.java
@@ -1,23 +1,50 @@
 package com.board.global.security.config;
 
+import com.board.domain.token.service.TokenService;
+import com.board.global.security.handler.MemberLoginFailureHandler;
+import com.board.global.security.handler.MemberLoginSuccessHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper;
+    private final TokenService tokenService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .formLogin(form -> form
+                        .loginProcessingUrl("/api/members/login")
+                        .usernameParameter("username")
+                        .passwordParameter("password")
+                        .successHandler(memberLoginSuccessHandler())
+                        .failureHandler(memberLoginFailureHandler())
+                        .permitAll()
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET,
                                 "/api/members/nickname/*",
@@ -32,6 +59,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler memberLoginSuccessHandler() {
+        return new MemberLoginSuccessHandler(objectMapper, tokenService);
+    }
+
+    @Bean
+    public AuthenticationFailureHandler memberLoginFailureHandler() {
+        return new MemberLoginFailureHandler(objectMapper);
     }
 
 }
