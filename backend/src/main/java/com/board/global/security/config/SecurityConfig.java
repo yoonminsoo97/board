f2bd88bb437c545f15a3dc4dc53084d5e5db69b6
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-18 16:51:59 +0900

    feat: 시큐리티 설정 추가

---
+++ backend/src/main/java/com/board/global/security/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.board.global.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/members/nickname/*",
+                                "/api/members/username/*").permitAll()
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/members/signup").permitAll()
+                        .anyRequest().denyAll()
+                );
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
