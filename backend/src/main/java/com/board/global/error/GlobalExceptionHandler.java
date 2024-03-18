d2a08725a7d484e749859dcd3d371c0342750b98
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-18 16:51:50 +0900

    feat: dto validation 예외 처리 핸들러 추가

--- backend/src/main/java/com/board/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/board/global/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.board.global.error.dto.ErrorResponse;
 import com.board.global.error.exception.BaseException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,9 +12,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BaseException.class)
-    ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
+    public ResponseEntity<ErrorResponse> handleBaseException(BaseException e) {
         final ErrorResponse errorResponse = ErrorResponse.of(e.getErrorType());
         return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException() {
+        final ErrorResponse errorResponse = ErrorResponse.of(ErrorType.INVALID_INPUT_VALUE);
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
 }
