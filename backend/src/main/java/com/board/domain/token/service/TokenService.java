316f412659e4eeb42059fe52815d54dc2da9e541
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-18 21:11:55 +0900

    feat: 토큰 저장 기능 추가

---
+++ backend/src/main/java/com/board/domain/token/service/TokenService.java
@@ -0,0 +1,33 @@
+package com.board.domain.token.service;
+
+import com.board.domain.member.entity.Member;
+import com.board.domain.token.dto.TokenResponse;
+import com.board.domain.token.entity.Token;
+import com.board.domain.token.repository.TokenRepository;
+import com.board.domain.token.util.JwtUtil;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final TokenRepository tokenRepository;
+    private final JwtUtil jwtUtil;
+
+    @Transactional
+    public TokenResponse tokenSave(Member member) {
+        String accessToken = jwtUtil.createAccessToken(member.getUsername(), member.getNickname(), member.getRole().getAuthority());
+        String refreshToken = jwtUtil.createRefreshToken(member.getUsername());
+        Token token = Token.builder()
+                .refreshToken(refreshToken)
+                .member(member)
+                .build();
+        tokenRepository.save(token);
+        return new TokenResponse(accessToken, refreshToken);
+    }
+
+}
