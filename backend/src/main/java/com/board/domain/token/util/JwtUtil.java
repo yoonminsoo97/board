3e2cf42a2afd131a25a642452a50b1c882e99024
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-18 21:11:43 +0900

    feat: access token, refresh token 생성 기능 추가

---
+++ backend/src/main/java/com/board/domain/token/util/JwtUtil.java
@@ -0,0 +1,54 @@
+package com.board.domain.token.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+import java.nio.charset.StandardCharsets;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+    private final long accessTokenExpire;
+    private final long refreshTokenExpire;
+
+    public JwtUtil(@Value("${jwt.secret-key}") String secretKey,
+                   @Value("${jwt.access-token.expire}") long accessTokenExpire,
+                   @Value("${jwt.refresh-token.expire}") long refreshTokenExpire) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.accessTokenExpire = accessTokenExpire;
+        this.refreshTokenExpire = refreshTokenExpire;
+    }
+
+    public String createAccessToken(String username, String nickname, String authority) {
+        Date iat = new Date();
+        Date exp = new Date(iat.getTime() + accessTokenExpire);
+        return Jwts.builder()
+                .subject(username)
+                .claim("nickname", nickname)
+                .claim("authority", authority)
+                .issuedAt(iat)
+                .expiration(exp)
+                .signWith(secretKey, Jwts.SIG.HS256)
+                .compact();
+    }
+
+    public String createRefreshToken(String username) {
+        Date iat = new Date();
+        Date exp = new Date(iat.getTime() + refreshTokenExpire);
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(iat)
+                .expiration(exp)
+                .signWith(secretKey, Jwts.SIG.HS256)
+                .compact();
+    }
+
+}
