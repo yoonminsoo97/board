6af9f56b532a45b494be98edf3336332b00ef38f
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-19 20:13:18 +0900

    feat: 토큰 무효, 만료 예외 추가

---
+++ backend/src/main/java/com/board/domain/token/exception/ExpiredTokenException.java
@@ -0,0 +1,13 @@
+package com.board.domain.token.exception;
+
+import com.board.global.error.ErrorType;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class ExpiredTokenException extends AuthenticationException {
+
+    public ExpiredTokenException() {
+        super(ErrorType.EXPIRED_TOKEN.getErrorCode());
+    }
+
+}
