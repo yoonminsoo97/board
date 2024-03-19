2dcff3be20f22a2102e5f2a33605c503d2838b96
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-19 22:05:15 +0900

    feat: authentication 예외를 처리하는 핸들러 추가

--- backend/src/main/java/com/board/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/board/global/error/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.board.global.error.dto.ErrorResponse;
 import com.board.global.error.exception.BaseException;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,6 +22,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException() {
         final ErrorResponse errorResponse = ErrorResponse.of(ErrorType.INVALID_INPUT_VALUE);
         return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        final ErrorResponse errorResponse = ErrorResponse.of(ErrorType.of(e.getMessage()));
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
     }
 
 }
