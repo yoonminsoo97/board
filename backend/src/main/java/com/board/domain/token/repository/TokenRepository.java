bffc688a105d34277f57e61439c1ef1e8fdcd1c9
yoonminsoo97 <yoonminwater@gmail.com>
2024-03-19 22:04:43 +0900

    feat: 토큰 삭제 기능 추가

--- backend/src/main/java/com/board/domain/token/repository/TokenRepository.java
+++ backend/src/main/java/com/board/domain/token/repository/TokenRepository.java
@@ -3,6 +3,14 @@ package com.board.domain.token.repository;
 import com.board.domain.token.entity.Token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
+
+    @Query("SELECT t FROM Token AS t WHERE t.member.username = :username")
+    Optional<Token> findByMemberUsername(@Param("username") String username);
+
 }
