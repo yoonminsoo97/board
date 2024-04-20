dbeb1779d287c49ec5d1f69b8427da6257e37573
윤민수 <163730288+yoonminsoo97@users.noreply.github.com>
2024-04-20 23:20:15 +0900

    feat: 회원이 작성한 댓글목록 조회 기능 구현 (#62)

--- backend/src/main/java/com/board/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/board/domain/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.board.domain.member.controller;
 
+import com.board.domain.comment.dto.CommentListResponse;
+import com.board.domain.comment.service.CommentService;
 import com.board.domain.member.dto.MemberProfileResponse;
 import com.board.domain.member.dto.MemberSignupRequest;
 import com.board.domain.member.service.MemberService;
@@ -29,6 +31,7 @@ public class MemberController {
 
     private final MemberService memberService;
     private final PostService postService;
+    private final CommentService commentService;
 
     @GetMapping("/nickname/{nickname}")
     public ResponseEntity<ApiResponse<Void>> memberNicknameExists(@PathVariable("nickname") String nickname) {
@@ -62,6 +65,15 @@ public class MemberController {
         page = page <= 0 ? 0 : page - 1;
         PostListResponse postListResponse = postService.postListFromMember(page, username);
         return ResponseEntity.ok().body(ApiResponse.success(postListResponse));
+    }
+
+    @Secured("ROLE_MEMBER")
+    @GetMapping("/profile/comments")
+    public ResponseEntity<ApiResponse<CommentListResponse>> memberProfileCommentList(@RequestParam("page") int page,
+                                                                                     @AuthenticationPrincipal String username) {
+        page = page <= 0 ? 0 : page - 1;
+        CommentListResponse commentListResponse = commentService.commentListFromMember(page, username);
+        return ResponseEntity.ok().body(ApiResponse.success(commentListResponse));
     }
 
 }
