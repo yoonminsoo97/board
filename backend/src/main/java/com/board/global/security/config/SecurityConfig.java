06c109dfae727c7ab45a57b0ef1c281eef391e2c
yoonminsoo97 <yoonminwater@gmail.com>
2024-04-20 23:18:44 +0900

    feat: 자신이 작성한 댓글 목록 조회 기능 추가

--- backend/src/main/java/com/board/global/security/config/SecurityConfig.java
+++ backend/src/main/java/com/board/global/security/config/SecurityConfig.java
@@ -78,7 +78,8 @@ public class SecurityConfig {
                                 "/api/tokens/reissue").permitAll()
                         .requestMatchers(HttpMethod.GET,
                                 "/api/members/profile",
-                                "/api/members/profile/posts").hasRole(ROLE_MEMBER)
+                                "/api/members/profile/posts",
+                                "/api/members/profile/comments").hasRole(ROLE_MEMBER)
                         .requestMatchers(HttpMethod.POST,
                                 "/api/posts/write",
                                 "/api/posts/*/comments/write").hasRole(ROLE_MEMBER)
