25524abeeea86684392fd2332e84e54480f79b27
yoonminsoo97 <yoonminwater@gmail.com>
2024-05-21 00:18:00 +0900

    feat: 대댓글 엔티티 및 관련 클래스 제거

--- backend/src/main/java/com/board/domain/reply/repository/ReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.board.domain.reply.repository;
-
-import com.board.domain.reply.entity.Reply;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
