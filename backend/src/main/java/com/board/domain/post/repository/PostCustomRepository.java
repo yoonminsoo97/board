faf1246d357a829eddca1d6220ac346fa6170423
윤민수 <163730288+yoonminsoo97@users.noreply.github.com>
2024-06-06 00:18:51 +0900

    refactor: 게시글 목록조회 기능 변경 (#87)

--- backend/src/main/java/com/board/domain/post/repository/PostCustomRepository.java
+++ backend/src/main/java/com/board/domain/post/repository/PostCustomRepository.java
@@ -1,14 +1,12 @@
 package com.board.domain.post.repository;
 
-import com.board.domain.post.dto.PostListItem;
+import com.board.domain.post.dto.PostListResponse;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PostCustomRepository {
 
-    Page<PostListItem> findPosts(Pageable pageable);
-    Page<PostListItem> findPostsSearch(Pageable pageable, String type, String keyword);
-    Page<PostListItem> findPostsFromMember(Pageable pageable, String username);
+    PostListResponse findPosts(Pageable pageable);
+    PostListResponse findSearchPosts(Pageable pageable, String type, String keyword);
 
 }
