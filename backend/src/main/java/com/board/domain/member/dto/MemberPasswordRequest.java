422db891f1d0c5556e7cd265f89f82835de4b2b8
yoonminsoo97 <yoonminwater@gmail.com>
2024-06-14 00:07:54 +0900

    feat: builder 적용

--- backend/src/main/java/com/board/domain/member/dto/MemberPasswordRequest.java
+++ backend/src/main/java/com/board/domain/member/dto/MemberPasswordRequest.java
@@ -2,13 +2,16 @@ package com.board.domain.member.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberPasswordRequest {
 
     @NotBlank(message = "현재 비밀번호를 입력해 주세요.")
