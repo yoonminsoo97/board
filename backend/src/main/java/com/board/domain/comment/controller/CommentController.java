46d9b45097f0bb2a07937221fb8de90790824522
윤민수 <163730288+yoonminsoo97@users.noreply.github.com>
2024-06-20 08:36:47 +0900

    feat: LoginMember 어노테이션 추가 (#101)

--- backend/src/main/java/com/board/domain/comment/controller/CommentController.java
+++ backend/src/main/java/com/board/domain/comment/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.board.domain.comment.dto.CommentModifyRequest;
 import com.board.domain.comment.dto.CommentWriteRequest;
 import com.board.domain.comment.service.CommentService;
 import com.board.global.common.dto.ApiResponse;
+import com.board.global.security.annotation.LoginMember;
 
 import jakarta.validation.Valid;
 
@@ -12,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +34,7 @@ public class CommentController {
     @PostMapping("/{postId}/comments")
     public ResponseEntity<ApiResponse<Void>> commentWrite(@PathVariable("postId") Long postId,
                                                           @RequestBody @Valid CommentWriteRequest commentWriteRequest,
-                                                          @AuthenticationPrincipal Long memberId) {
+                                                          @LoginMember Long memberId) {
         commentService.commentWrite(postId, commentWriteRequest, memberId);
         return ResponseEntity.ok().body(ApiResponse.success());
     }
@@ -44,7 +44,7 @@ public class CommentController {
     public ResponseEntity<ApiResponse<Void>> replyWrite(@PathVariable("postId") Long postId,
                                                         @PathVariable("commentId") Long commentId,
                                                         @RequestBody @Valid CommentWriteRequest commentWriteRequest,
-                                                        @AuthenticationPrincipal Long memberId) {
+                                                        @LoginMember Long memberId) {
         commentService.replyWrite(postId, commentId, commentWriteRequest, memberId);
         return ResponseEntity.ok().body(ApiResponse.success());
     }
@@ -62,7 +62,7 @@ public class CommentController {
     public ResponseEntity<ApiResponse<Void>> commentModify(@PathVariable("postId") Long postId,
                                                            @PathVariable("commentId") Long commentId,
                                                            @RequestBody @Valid CommentModifyRequest commentModifyRequest,
-                                                           @AuthenticationPrincipal Long memberId) {
+                                                           @LoginMember Long memberId) {
         commentService.commentModify(postId, commentId, commentModifyRequest, memberId);
         return ResponseEntity.ok().body(ApiResponse.success());
     }
@@ -71,7 +71,7 @@ public class CommentController {
     @DeleteMapping("/{postId}/comments/{commentId}")
     public ResponseEntity<ApiResponse<Void>> commentDelete(@PathVariable("postId") Long postId,
                                                            @PathVariable("commentId") Long commentId,
-                                                           @AuthenticationPrincipal Long memberId) {
+                                                           @LoginMember Long memberId) {
         commentService.commentDelete(postId, commentId, memberId);
         return ResponseEntity.ok().body(ApiResponse.success());
     }
