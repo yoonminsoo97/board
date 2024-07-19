31419af222ef42448f3f033687cf1f9f9f2a8780
yoonminsoo97 <yoonminwater@gmail.com>
2024-07-19 21:33:07 +0900

    feat: 삭제할 게시글에 포함된 모든 댓글 삭제 로직 추가

--- backend/src/main/java/com/board/domain/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/board/domain/comment/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package com.board.domain.comment.repository;
 import com.board.domain.comment.entity.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,5 +13,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Comment
 
     @Query("SELECT c FROM Comment AS c WHERE c.post.id = :postId AND c.id = :commentId")
     Optional<Comment> findCommentByPostIdAndCommentId(@Param("postId") Long postId, @Param("commentId") Long commentId);
+
+    @Modifying
+    @Query("DELETE FROM Comment AS c WHERE c.post.id = :postId")
+    void deleteByPostId(@Param("postId") Long postId);
 
 }
