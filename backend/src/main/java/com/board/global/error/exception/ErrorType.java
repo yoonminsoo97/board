5fc790740406d77b3f6d2eba2edbaeb9c8305044
윤민수 <yoonminwater@gmail.com>
2024-12-20 00:36:14 +0900

    feat: 전역 예외 처리 핸들러 추가 (#5)

---
+++ backend/src/main/java/com/board/global/error/exception/ErrorType.java
@@ -0,0 +1,22 @@
+package com.board.global.error.exception;
+
+import lombok.Getter;
+
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorType {
+
+    ;
+
+    private final HttpStatus status;
+    private final String errorCode;
+    private final String message;
+
+    ErrorType(HttpStatus status, String errorCode, String message) {
+        this.status = status;
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+}
