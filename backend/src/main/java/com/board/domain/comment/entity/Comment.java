e8771879920e11fe68484647841c1b88d75b8b12
yoonminsoo97 <yoonminwater@gmail.com>
2025-02-04 13:12:37 +0900

    feat: 회원, 게시글, 댓글, 토큰 엔티티 추가

---
+++ backend/src/main/java/com/board/domain/comment/entity/Comment.java
@@ -0,0 +1,52 @@
+package com.board.domain.comment.entity;
+
+import com.board.domain.member.entity.Member;
+import com.board.domain.post.entity.Post;
+import com.board.global.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Builder
+    public Comment(String writer, String content, Member member, Post post) {
+        this.writer = writer;
+        this.content = content;
+        this.member = member;
+        this.post = post;
+    }
+
+}
