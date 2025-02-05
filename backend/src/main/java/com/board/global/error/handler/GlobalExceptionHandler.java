75a1d92f3762004f12e943aea7cccf7e0404542f
yoonminsoo97 <yoonminwater@gmail.com>
2025-02-05 17:48:29 +0900

    feat: 유효성 검증 예외 처리 핸들러 추가

--- backend/src/main/java/com/board/global/error/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/board/global/error/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import com.board.global.error.exception.APIException;
 import com.board.global.error.exception.ErrorType;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,6 +17,12 @@ public class GlobalExceptionHandler {
         ErrorType errorType = ex.getErrorType();
         ErrorResponse errorResponse = ErrorResponse.of(errorType);
         return ResponseEntity.status(errorType.getHttpStatus()).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException ex) {
+        ErrorResponse errorResponse = ErrorResponse.of(ErrorType.INVALID_INPUT_VALUE, ex.getFieldErrors());
+        return ResponseEntity.badRequest().body(errorResponse);
     }
 
 }
