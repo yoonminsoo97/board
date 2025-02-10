53defb6507e7f0a03c3c94ce93028960f04c050e
yoonminsoo97 <yoonminwater@gmail.com>
2025-02-10 23:46:21 +0900

    feat: 게시글 수정 기능 추가

--- backend/src/main/java/com/board/domain/post/service/PostService.java
+++ backend/src/main/java/com/board/domain/post/service/PostService.java
@@ -3,8 +3,10 @@ package com.board.domain.post.service;
 import com.board.domain.member.entity.Member;
 import com.board.domain.member.exception.NotFoundMemberException;
 import com.board.domain.member.repository.MemberRepository;
+import com.board.domain.post.dto.PostModifyRequest;
 import com.board.domain.post.dto.PostWriteRequest;
 import com.board.domain.post.entity.Post;
+import com.board.domain.post.exception.NotFoundPostException;
 import com.board.domain.post.repository.PostRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -30,6 +32,13 @@ public class PostService {
                 .content(postWriteRequest.getContent())
                 .build();
         postRepository.save(post);
+    }
+
+    @Transactional
+    public void postModify(Long postId, PostModifyRequest postModifyRequest) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(NotFoundPostException::new);
+        post.modify(postModifyRequest.getTitle(), postModifyRequest.getContent());
     }
 
 }
