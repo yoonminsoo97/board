f5ab0ff2061fc27f4227b6317ceae44f8f90d922
yoonminsoo97 <yoonminwater@gmail.com>
2025-02-13 16:14:14 +0900

    feat: 댓글 작성 요청 권한 회원으로 설정

--- backend/src/main/java/com/board/global/security/config/SecurityConfig.java
+++ backend/src/main/java/com/board/global/security/config/SecurityConfig.java
@@ -62,7 +62,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST,
                                 "/api/members/signup").permitAll()
                         .requestMatchers(HttpMethod.POST,
-                                "/api/posts/write").hasRole("MEMBER")
+                                "/api/posts/write",
+                                "/api/posts/*/comments/write").hasRole("MEMBER")
                         .requestMatchers(HttpMethod.PUT,
                                 "/api/posts/*").hasRole("MEMBER")
                         .requestMatchers(HttpMethod.DELETE,
