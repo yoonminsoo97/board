0abf4e04900bccf302de6b89b0f01d4ba5c1d565
yoonminsoo97 <yoonminwater@gmail.com>
2025-02-13 16:14:04 +0900

    feat: 댓글 작성 기능 추가

---
+++ backend/src/main/java/com/board/domain/comment/service/CommentService.java
@@ -0,0 +1,41 @@
+package com.board.domain.comment.service;
+
+import com.board.domain.comment.dto.CommentWriteRequest;
+import com.board.domain.comment.entity.Comment;
+import com.board.domain.comment.repository.CommentRepository;
+
+import com.board.domain.member.entity.Member;
+import com.board.domain.member.exception.NotFoundMemberException;
+import com.board.domain.member.repository.MemberRepository;
+import com.board.domain.post.entity.Post;
+import com.board.domain.post.exception.NotFoundPostException;
+import com.board.domain.post.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final PostRepository postRepository;
+    private final MemberRepository memberRepository;
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public void commentWrite(Long postId, String username, CommentWriteRequest commentWriteRequest) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(NotFoundPostException::new);
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(NotFoundMemberException::new);
+        Comment comment = Comment.builder()
+                .content(commentWriteRequest.getContent())
+                .member(member)
+                .post(post)
+                .build();
+        commentRepository.save(comment);
+    }
+
+}
