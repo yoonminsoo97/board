d7217983009e844daa8f8ca7382ee03e499b3118
yoonminsoo97 <yoonminwater@gmail.com>
2025-02-15 21:26:45 +0900

    feat: 댓글 수정 기능 추가

--- backend/src/main/java/com/board/domain/comment/controller/CommentController.java
+++ backend/src/main/java/com/board/domain/comment/controller/CommentController.java
@@ -1,5 +1,6 @@
 package com.board.domain.comment.controller;
 
+import com.board.domain.comment.dto.CommentModifyRequest;
 import com.board.domain.comment.dto.CommentWriteRequest;
 import com.board.domain.comment.service.CommentService;
 
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,14 @@ public class CommentController {
                                              @AuthenticationPrincipal String username,
                                              @RequestBody @Valid CommentWriteRequest commentWriteRequest) {
         commentService.commentWrite(postId, username, commentWriteRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<Void> commentModify(@PathVariable("postId") Long postId,
+                                              @PathVariable("commentId") Long commentId,
+                                              @RequestBody @Valid CommentModifyRequest commentModifyRequest) {
+        commentService.commentModify(postId, commentId, commentModifyRequest);
         return ResponseEntity.ok().build();
     }
 
--- backend/src/main/java/com/board/domain/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/board/domain/comment/repository/CommentRepository.java
@@ -4,5 +4,10 @@ import com.board.domain.comment.entity.Comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    Optional<Comment> findByPostIdAndId(Long postId, Long commentId);
+
 }
