d0ebe2ec5cc2f6aedc89cdae7d7c7e163919fbf3
yoonminsoo97 <yoonminwater@gmail.com>
2025-03-02 14:39:18 +0900

    feat: 게시글 작성 요청 권한 MEMBER 설정

--- backend/src/main/java/com/backend/global/security/filter/AuthenticationFilter.java
+++ backend/src/main/java/com/backend/global/security/filter/AuthenticationFilter.java
@@ -64,7 +64,8 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
         MEMBER_SIGNUP(HttpMethod.POST, "/api/members/signup", Authority.PERMIT_ALL),
         MEMBER_LOGIN(HttpMethod.POST, "/api/auth/login", Authority.PERMIT_ALL),
-        MEMBER_LOGOUT(HttpMethod.POST, "/api/auth/logout", Authority.ROLE_MEMBER);
+        MEMBER_LOGOUT(HttpMethod.POST, "/api/auth/logout", Authority.ROLE_MEMBER),
+        POST_WRITE(HttpMethod.POST, "/api/posts/write", Authority.ROLE_MEMBER);
 
         enum Authority {
             PERMIT_ALL, ROLE_MEMBER
