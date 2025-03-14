1a7001637c23b6af3c238513470aa0ef59c2fa3b
윤민수 <yoonminwater@gmail.com>
2025-03-14 22:49:16 +0900

    feat: 게시글 삭제 시 댓글 삭제 로직 추가 (#43)

--- backend/src/main/java/com/backend/domain/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/backend/domain/comment/repository/CommentRepository.java
@@ -5,6 +5,7 @@ import com.backend.domain.comment.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,6 +15,10 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("SELECT c FROM Comment AS c WHERE c.post.id = :postId AND c.id = :commentId")
     Optional<Comment> findByPostIdAndCommentId(@Param("postId") Long postId, @Param("commentId") Long commentId);
+
+    @Query("DELETE FROM Comment AS c WHERE c.post.id = :postId")
+    @Modifying
+    void deleteByPostId(@Param("postId") Long postId);
 
     Page<Comment> findAllByPostId(Long postId, Pageable pageable);
 
